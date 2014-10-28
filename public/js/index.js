fc4bc94df729e82841198c5ab0356d0cbd5ea592
pachewise <pachewise@gmail.com>
2014-10-28 01:16:08 -0400

    smooth scroll

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('a[href*=#]').click(function(event){
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 600);
+  });
+});
+
