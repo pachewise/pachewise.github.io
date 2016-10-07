6c38d3b3721874c46769347730bd3be10d5fd42e
pachewise <pachewise@gmail.com>
2016-10-07 13:02:11 -0700

    js to scroll, keep navbar... css changes to make sections look prettier

--- public/js/index.js
+++ public/js/index.js
@@ -12,37 +12,49 @@ $(document).ready(function () {
 
         var target = this.hash;
         $target = $(target);
+        var offset = $target.offset().top+2;
+        if (target === "#top") {
+            offset = 0;
+            $('nav').hide();
+        }
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top-$('nav').outerHeight()+2
+            'scrollTop': offset
         }, 500, 'swing', function () {
             window.location.hash = target;
+            checkSticky();
             $(document).on("scroll", onScroll);
         });
     });
 });
 
 function onScroll(event){
+    var scrollPosition = $(document).scrollTop();
+    checkSticky();
+
+    $('nav a').each(function () {
+        var currentLink = $(this);
+        var refElement = $(currentLink.attr("href"));
+        if (refElement.selector === "#top") return;
+        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+            $('.block').removeClass("active");
+            refElement.addClass("active");
+            //window.location.hash = refElement.selector;
+        } else {
+            refElement.removeClass("active");
+        }
+    });
+}
+
+function checkSticky($t) {
     var mn = $("nav");
+    mn.show();
     var mns = "sticky-nav";
-    var hdr = $('header').height();
-    if( $(this).scrollTop() > hdr ) {
+    var hdr = $('header').outerHeight();
+    if( $(document).scrollTop() > hdr ) {
         mn.addClass(mns);
     } else {
         mn.removeClass(mns);
         $('nav ul li a').removeClass("active");
+        window.location.hash = "";
     }
-    var scrollPosition = $(document).scrollTop();
-    $('nav a').each(function () {
-        var navHeight = $('nav').outerHeight();
-        var currentLink = $(this);
-        var refElement = $(currentLink.attr("href"));
-        if (refElement.selector === "#top") return;
-        if (refElement.position().top <= scrollPosition + navHeight && refElement.position().top + refElement.height() > scrollPosition + navHeight) {
-            $('nav ul li a').removeClass("active");
-            currentLink.addClass("active");
-            window.location.hash = refElement.selector;
-        } else {
-            currentLink.removeClass("active");
-        }
-    });
 }
