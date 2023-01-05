3aa1817b6d0517bdab0015f6262765950ae7252d
José Pacheco <pachewise@gmail.com>
2023-01-05 18:35:22 -0500

    patch liquidjs vuln

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPassthroughCopy("assets/img");
   eleventyConfig.addPassthroughCopy("CNAME");
+
+  // CVE-2022-25948, see https://github.com/advisories/GHSA-45rm-2893-5f49
+  // fixed in liquidjs > 10.0.0
+  eleventyConfig.setLiquidOptions({
+    ownPropertyOnly: true,
+  });
   return {
     dir: {
       input: "./",
