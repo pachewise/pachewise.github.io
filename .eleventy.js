ebdb994eff7ec809eeb0130ddd7fc6b81269ee1a
José Pacheco <pachewise@gmail.com>
2024-09-11 23:30:27 -0400

    fix sass, img

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,39 @@
+const sass = require("sass");
+const path = require("node:path");
 const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets/img");
+  eleventyConfig.addTemplateFormats("scss");
+  // Creates the extension for use
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css", // optional, default: "html"
+
+		// `compile` is called once per .scss file in the input directory
+		compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [
+          parsed.dir || ".",
+          this.config.dir.includes
+        ]
+      });
+			// This is the render function, `data` is the full data cascade
+			return async (data) => {
+				return result.css;
+			};
+    },
+    compileOptions: {
+      permalink: function (contents, inputPath) {
+        let parsed = path.parse(inputPath);
+        if (parsed.name.startsWith("_")) {
+          return false;
+        }
+      }
+    }
+	});
+  eleventyConfig.addPassthroughCopy("src/assets/img");
   eleventyConfig.addPassthroughCopy("CNAME");
 
   eleventyConfig.addPlugin(UpgradeHelper);
