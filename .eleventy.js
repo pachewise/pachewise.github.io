6e79c07b6c9767414be934051b6675ad2489d7dd
José Pacheco <pachewise@gmail.com>
2024-09-11 23:13:36 -0400

    build with eleventy 3.0.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,15 @@
-const sass = require("sass");
-const eleventySass = require("eleventy-sass");
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPassthroughCopy("assets/img");
   eleventyConfig.addPassthroughCopy("CNAME");
 
-  // CVE-2022-25948, see https://github.com/advisories/GHSA-45rm-2893-5f49
-  // fixed in liquidjs > 10.0.0
-  eleventyConfig.setLiquidOptions({
-    ownPropertyOnly: true,
-  });
+  eleventyConfig.addPlugin(UpgradeHelper);
+
   return {
     dir: {
-      input: "./",
+      input: ".",
       output: "./_site",
       layouts: "./_layouts",
     },
