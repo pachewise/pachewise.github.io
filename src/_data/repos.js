f120c06d993f255fcf745934d0cde58fd977bf57
José Pacheco <pachewise@gmail.com>
2025-06-26 08:49:29 -0400

    change eleventy-cache-assets -> eleventy-fetch (new name, version)

--- src/_data/repos.js
+++ src/_data/repos.js
@@ -1,17 +1,17 @@
 // original source: https://github.com/maxboeck/resume/blob/9c177fcb8abc315514e57e2701152871a529e2be/src/data/repositories.js
-import Cache from '@11ty/eleventy-cache-assets';
+import Fetch from "@11ty/eleventy-fetch";
 
 const GITHUB_USERNAME = 'pachewise';
 
 export default async function () {
     try {
         console.log('Fetching GitHub repos...');
-        const repos = await Cache(
-            `https://api.github.com/users/${GITHUB_USERNAME}/starred?per_page=5&sort=stars&direction=desc`,
-            {
-                duration: '1d',
-                type: 'json'
-            }
+        const repos = await Fetch(
+          `https://api.github.com/users/${GITHUB_USERNAME}/starred?per_page=5&sort=stars&direction=desc`,
+          {
+            duration: "1d",
+            type: "json",
+          }
         );
         return repos;
     } catch (e) {
